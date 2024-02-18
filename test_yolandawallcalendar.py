09480ddaa52ebe38ec5587cc097d02a18b33a56a
Yolanda Hernandez <yolanda_016@hotmail.com>
2024-02-18 15:12:00 +0100

    gettingback

--- test_yolandawallcalendar.py
+++ test_yolandawallcalendar.py
@@ -1,14 +1,9 @@
 from yolanda_wall_calendar import wall_calendar
 
 def test_crear_calendario_fecha_valida():
-    
-    wall_calendar()
-    
+
     date = wall_calendar(1989, 11, 13)
-    
 
-    date_str = str(date)
-    
+    assert date == "Lunes, 13 de noviembre de 1989"
 
-    assert date_str == "Lunes, 13 de noviembre de 1989"
 
