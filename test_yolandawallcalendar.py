822f8093e3fb634f309a9e20452ed0085ff75e72
Yolanda Hernandez <yolanda_016@hotmail.com>
2024-02-18 15:09:40 +0100

    correctfunctionimport

--- test_yolandawallcalendar.py
+++ test_yolandawallcalendar.py
@@ -1,8 +1,14 @@
-from yolanda_calendar import wall_calendar
+from yolanda_wall_calendar import wall_calendar
 
 def test_crear_calendario_fecha_valida():
-    date = wall_calendar(1989, 11, 13)
     
     wall_calendar()
     
-    assert wall_calendar(1989, 11, 13) == "Lunes, 13 de noviembre de 1989"
+    date = wall_calendar(1989, 11, 13)
+    
+
+    date_str = str(date)
+    
+
+    assert date_str == "Lunes, 13 de noviembre de 1989"
+
